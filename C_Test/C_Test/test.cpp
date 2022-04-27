cf49ae259fa674f51b87de8610e31bc0bf60a836
TreePer <treeper0552@gmail.com>
2022-04-27 20:49:35 +0900

    0.001

--- C_Test/C_Test/test.cpp
+++ C_Test/C_Test/test.cpp
@@ -6,57 +6,265 @@
 #include <Windows.h>
 
 // 상수 선언
+
 const int PLAYER = 0;
 const int ENEMY = 1;
 const int MAX = 2;
 
-// 플레이어, 몬스터의 정보를 넣을 구조체 선언
+const int Scene_Logo = 0;
+const int Scene_Menu = 1;
+const int Scene_Stage = 2;
+const int Scene_Exit = 3;
+
+// 전역 변수 선언
+
+int SceneState = 0;
+
+int Check = 1;
+
+
+// 구조체 선언
+
 typedef struct tagInfo {
 	char* Name;
-	int HP;
-	int MP;
 
+	int HP;
+	
 	int EXP;
 
 	float Att;
 	float Def;
+	float speed;
 
 	int Level;
 
+	char* skillname1;
+	int skillpp1;
+	int skillpoint1;
+
+	char* skillname2;
+	int skillpp2;
+	int skillpoint2;
+
+	char* skillname3;
+	int skillpp3;
+	int skillpoint3;
+
+	char* skillname4;
+	int skillpp4;
+	int skillpoint4;
+
 }INFO;
 
-// tagInfo(INFO)구조체를 변수로 가지는 구조체 선언
+typedef struct userInfo {
+	char* userName;
+
+	INFO info1;
+	INFO info2;
+	INFO info3;
+	INFO info4;
+	INFO info5;
+	INFO info6;
+}USER;
+
 typedef struct tagObject {
-	INFO Info;
+	USER user;
 
 }OBJECT;
 
+// 기능 정리 - 유저생성(이름, 초기포켓몬), 전투(데미지계산, 포켓몬스탯비교, 스킬포인트사용, 아이템사용), 도망(랜덤), 
+//             이동(키입력? 문자입력?), 회복(특정장소로이동 후 한번에 회복), 포획(현재 체력 비교 + 랜덤)
 
 
-// 함수 전방선언
-void InitializeObject(OBJECT* _Obj, int ObjectType);
+
+
+void SceneManager(OBJECT* _Player);
 char* SetName();
 
+void LogoScene();
+void MenuScene();
+void FirstScene();
+void StageScene(OBJECT* _Player);
+
+void InitializePlayer(OBJECT* _Player);
+void PlayerScene(OBJECT* _Player);
+
+void InitializeEnemy(OBJECT* _Enemy);
+void EnemyScene(OBJECT* _Enemy);
+
+void SetPosition(int _x, int _y, char* _str, int _Color = 15);
+void SetColor(int _Color);
+void VisibleCursor();
+void InvisibleCursor();
 
 int main(void) {
-	/*
+	
+	system("mode con: cols=150 lines=60");
+	system("title 김동욱 C Text Pokemon");
+
 	OBJECT* Player = (OBJECT*)malloc(sizeof(OBJECT));
-	InitializeObject(Player, TYPE_PLAYER);
-	OBJECT* Enemy = (OBJECT*)malloc(sizeof(OBJECT));;
-	InitializeObject(Enemy, TYPE_ENEMY);
-	*/
+	InitializePlayer(Player);
 
-	// 구조체 포인터 생성
-	OBJECT* Objects[MAX];
-	// 구조체 플레이어 초기화
-	Objects[PLAYER] = (OBJECT*)malloc(sizeof(OBJECT));
-	InitializeObject(Objects[PLAYER], PLAYER);
-	// 구조체 몬스터 초기화
-	Objects[ENEMY] = (OBJECT*)malloc(sizeof(OBJECT));
-	InitializeObject(Objects[ENEMY], ENEMY);
-	// 도망기능에서 사용할 랜덤 변수 선언
-	int dice;
+	printf_s("%s \n", Player->user.userName);
 
+	while (true) {
+		SceneManager(Player);
+	}
+	
+
+	return 0;
+}
+
+
+void SceneManager(OBJECT* _Player) {
+	switch (SceneState)
+	{
+	case Scene_Logo:
+		LogoScene();
+		break;
+	case Scene_Menu:
+		MenuScene();
+		break;
+	case Scene_Stage:
+		StageScene(_Player);
+		break;
+	case Scene_Exit:
+
+		exit(NULL);// ** 프로그램 종료
+		break;
+	}
+}
+
+char* SetName() {
+	// ** scanf 함수로 문자열을 입력받기 위해 문자열을 받을수있는 배열을 선언.
+	char Buffer[128] = "";
+
+	printf_s("이름 입력 : ");
+
+	// ** 문자열을 입력 받음.
+	scanf("%s", Buffer);
+
+	// ** 입력 받은값은 배열 이지만, 반환 값은 캐릭터 포인터형이므로 문자열을 복사햐야함.
+	// ** 문자열을 복사 하기위해 포인터가 가르키는 공간에 입력받은 문자열이 들아갈만큼의 크기로 메모리 할당.
+	char* pName = (char*)malloc(strlen(Buffer) + 1);
+
+	// ** Buffer 가 받은 문자열을 pName 으로 복사.
+	strcpy(pName, Buffer);
+
+	// ** 반환.
+	return pName;
+}
+
+void LogoScene() {
+	printf_s("logo test\n");
+
+	Sleep(500);
+	FirstScene();
+	SceneState++;
+}
+
+void MenuScene() {
+	printf_s("MenuScene\n");
+
+	printf_s("다음 씬 ㄱㄱ??\n1. 이동\n2. 종료\n입력 : ");
+
+	int i = 0;
+	scanf("%d", &i);
+
+	if (i == 1)
+		SceneState++;
+	else if (i == 2)
+		SceneState = Scene_Exit;
+}
+
+void FirstScene() {
+
+	Sleep(3000);
+	system("cls");
+	printf_s("대사변경테스트 1");
+
+	Sleep(3000);
+	system("cls");
+	printf_s("대사변경테스트 2");
+
+	Sleep(3000);
+	system("cls");
+	printf_s("대사변경테스트 3");
+
+	Sleep(3000);
+	system("cls");
+	printf_s("대사변경테스트 4");
+
+	Sleep(3000);
+	system("cls");
+	printf_s("대사변경테스트 5\n");
+
+
+
+}
+
+void StageScene(OBJECT* _Player) {
+	// ** 전투
+	PlayerScene(_Player);
+	
+}
+
+void InitializePlayer(OBJECT* _Player) {
+	_Player->user.userName = SetName();
+
+}
+
+DWORD SetnameTime = 0;
+
+void PlayerScene(OBJECT* _Player) {
+	
+}
+
+void InitializeEnemy(OBJECT* _Enemy) {
+	
+}
+
+void EnemyScene(OBJECT* _Enemy) {
+
+}
+
+
+
+void SetPosition(int _x, int _y, char* _str, int _Color) {
+	COORD Pos = { _x, _y };
+
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+	SetColor(_Color);
+
+	printf_s("%s", _str);
+}
+
+
+void SetColor(int _Color) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), _Color);
+}
+
+
+
+void VisibleCursor() {
+	CONSOLE_CURSOR_INFO Info;
+
+	Info.dwSize = 1;
+	Info.bVisible = true;
+
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+}
+
+void InvisibleCursor() {
+	CONSOLE_CURSOR_INFO Info;
+
+	Info.dwSize = 1;
+	Info.bVisible = false;
+
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
+}
+
+/*
 
 	while (true) {
 		// ** 콘솔창을 모두 지움.
@@ -93,7 +301,7 @@ int main(void) {
 				printf_s("%d의 데미지를 몬스터에게 주었습니다.\n", (int)(Objects[PLAYER]->Info.Att - Objects[ENEMY]->Info.Def)); // 데미지를 표시 HP의 값이 int이므로 int로 형변환
 				Sleep(500);
 			}
-			else { 
+			else {
 				Objects[ENEMY]->Info.HP -= 1; // 공격력 < 방어력일 경우 1의 데미지를 줌
 				printf_s("1의 데미지를 몬스터에게 주었습니다.\n");
 				Sleep(500);
@@ -109,13 +317,13 @@ int main(void) {
 				printf_s("1의 데미지를 몬스터에게 받았습니다.\n");
 				Sleep(500);
 			}
-			
+
 			break;
 		case 2 : // '2'일경우
 			dice = (int)rand % 2; // 랜덤함수를 이용해 0, 1의 값이 나오게함
-			
+
 			if (dice) { // 1일경우
-				printf_s("도망치는것에 [성공] 했습니다.\n"); 
+				printf_s("도망치는것에 [성공] 했습니다.\n");
 				InitializeObject(Objects[ENEMY], ENEMY); // 도망치는것을 성공, 새로운 몬스터를 등장시킴
 				Sleep(500);
 			}
@@ -131,55 +339,11 @@ int main(void) {
 				}
 				Sleep(500);
 			}
-			
+
 			break;
 
 		default : // 다른 숫자를 입력했을경우
 			printf_s("잘못된 입력입니다. 다시 입력하세요. \n");
 			Sleep(500);
 		}
-	}
-
-	return 0;
-}
-
-
-
-// 플레이어와 몬스터의 데이터를 구조체에 등록시키는 함수
-void InitializeObject(OBJECT* _Obj, int ObjectType)
-{
-	switch (ObjectType) {
-	case PLAYER : // 플레이어일경우
-		_Obj->Info.Name = SetName(); // SetName함수에서 입력받은 이름을 저장
-
-		_Obj->Info.Att = 10;
-		_Obj->Info.Def = 10;
-		_Obj->Info.EXP = 0;
-		_Obj->Info.HP = 100;
-		_Obj->Info.MP = 10;
-		_Obj->Info.Level = 1;
-		break;
-	case ENEMY : // 몬스터일 경우
-		_Obj->Info.Name = (char*)"Enemy";
-
-		_Obj->Info.Att = 5;
-		_Obj->Info.Def = 5;
-		_Obj->Info.EXP = 0;
-		_Obj->Info.HP = 30;
-		_Obj->Info.MP = 5;
-		_Obj->Info.Level = 1;
-		break;
-	}
-}
-
-char* SetName() { // 이름을 입력받는 함수
-	char Buffer[128] = ""; // 이름을 입력받을 공간을 배열로 생성
-
-	printf_s("이름 입력 : ");
-	scanf("%s", Buffer);
-
-	char* pName = (char*)malloc(strlen(Buffer) + 1); // 포인터 변수에게 입력받은 이름의 글자수만큼 동적할당
-	strcpy(pName, Buffer); // 포인터에 이름을 전달함
-
-	return pName; // 입력받은 이름이 저장된 포인터 반환
-}
+*/
