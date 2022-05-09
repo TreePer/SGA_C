213ef9fcc48b2ded40e8220d9b757ff474ce3840
TreePer <treeper0552@gmail.com>
2022-05-09 22:57:44 +0900

    0.005

--- C_Test/C_Test/test.cpp
+++ C_Test/C_Test/test.cpp
@@ -33,7 +33,7 @@ typedef struct tagInfo {
 
 	int HP;
 	int MAXHP;
-	
+
 	int EXP;
 	int MAXEXP;
 
@@ -78,22 +78,20 @@ void InsertPokemon(OBJECT* _Player, OBJECT* _Enemy, int select);
 void SwitchPokemon(OBJECT* _Player);
 int RunBattle();
 
-void RecoveryPokemon(OBJECT*  _Player);
+void RecoveryPokemon(OBJECT* _Player);
 
 void InitializeEnemy(OBJECT* _Enemy);
-void EnemyScene(OBJECT* _Enemy);
 
-void SetPosition(int _x, int _y, char* _str, int _Color = 15);
+void SetPosition(int _x, int _y, char* _str, int _Color = 7);
 void SetColor(int _Color);
 void VisibleCursor();
 void InvisibleCursor();
 
 int main(void) {
-	
-	system("mode con: cols=150 lines=60");
 	system("title 김동욱 C Text Pokemon");
 
-	
+	LogoScene();
+
 	OBJECT* Player = (OBJECT*)malloc(sizeof(OBJECT));
 	InitializePlayer(Player);
 
@@ -101,7 +99,7 @@ int main(void) {
 		Player->user.info[i] = (INFO*)malloc(sizeof(INFO));
 		free(Player->user.info[i]);
 		Player->user.info[i] = nullptr;
-	}	
+	}
 
 	printf_s("플레이어 이름 %s \n", Player->user.userName);
 
@@ -110,7 +108,7 @@ int main(void) {
 	while (true) {
 		SceneManager(Player);
 	}
-	
+
 
 	return 0;
 }
@@ -120,7 +118,7 @@ void SceneManager(OBJECT* _Player) {
 	switch (SceneState)
 	{
 	case Scene_Logo:
-		LogoScene();
+		FirstScene();
 		FirstPokemon(_Player);
 		break;
 	case Scene_Menu:
@@ -157,16 +155,79 @@ char* SetName() {
 }
 
 void LogoScene() {
-	printf_s("logo test\n");
+	InvisibleCursor();
+	system("mode con:cols=150 lines=30");
+	system("color E");
 
+	char LOGO[28][151] = {
+		{"                                                                                WXOKW                                                                 "},
+		{"                                                                               WKdc:lON                                                               "},
+		{"                                                                              Nx:,:c::xX                                                              "},
+		{"                                                                            NOl;;lxxd::oKW                                                            "},
+		{"                                                         WWWWNNW         WKko:;cdO0O:::;oK                                                            "},
+		{"                   WWWNNNNWW                       WWXKOkdddoclkKW     WXxc;;:okkxol:::OKW         WNKOkOkkkkkOX                                      "},
+		{"            WNKOkxdoollllllodxOKNW           WNXKOxdoc:::;;'',::cd0N  Nx;,;:;:llc::::::  WXkddddddddl:;;:cccc:;xN       WXKKNNWW                      "},
+		{"       WNKOxoc::::clllooollcc:::lxKW       Xxllc::clooddl:;,;cool::oONXo'.,;;;;;::clloxOXXl',;:llllllc::okkkxl;oX      Nx;,:llodxkO0KXNW              "},
+		{"    WX0xl:::lodxkOOO0000OOOOkxdlc;:dXW    Wx'.,;lxkO000Oo:;:lk0Okdl::lxo:;cclooddddoolc:cl;';;lkOOOOko:cx000ko:c0W     Xc.,;clolccccclldkkkOKXNWW     "},
+		{"  Nkoc;:ldxOO00000000000000000Okxoc;lKW   Wk,';;:clk000Od::dO0000Okdc;;:ldkOOOOOOO0OOkxoc:;;;:d00000OdclO000Odc:xW     Xl',;coxkkkkxdoc:,,;:cclodxkOKN"},
+		{" No..,;cx0000000000000000OO00000Okoc;oX    O:,,,,;cx000Odox0000000ko:;cx00OOxolllcok00Odc;;;;ck00000Oxld0000Oxl;oX WNXKOl,,,;;oO0000Oxl:;;coxddool:;oX"},
+		{" WO:.',;:dO00000000000OxollloxO00Oxl;c0 WNX0xo;.';:d000OOO000000kdc;;:x00OdccdOklcdOOxl:;,',;lO000000kxk00000ko:cxkdllccc:;;;:d000000ko:::lk000Oko:cOW"},
+		{"   Kl'',;:dOOkO0000000Od::oo:;oO00kc;cxkdllccc:;;;:dO000000000Odccol;lO00kl;ckxloOOxl::::,,;:d00000000O000000Odc;:cododxxxdol:cxO0000Odc::d00000xc:xN "},
+		{"    Xd,,,;;cc;ck0000000kl:xKo;lO0Od:;;:codddxkxdoc:cxO000000Oxc;:dOd;lk00OdlccokOkoccoddl::;:x000000000000000kl:lxkxllxO00Okxoc:dO000Oxl:lk0000kl:lK  "},
+		{"     Nx:,;,,,;;lk000000Od:coccx0Od:;:okOkllxO00Okxoc:dO00000kdolccc:;:d000OOkkO0OxxkO00Okdl:;cx000kkO0000000xc:oO0x:;cxOOkoxOko:ck0000kdcd0000Od:cOW  "},
+		{"      WOloo;',,;lO000000Oo:cdk0ko:;cx00xc;cxO0kddOko:cx000000OOOkxdoc::ok00000000000000Oxoc:cxO00Oocx000kkOkl:lO0Odc:;:cc:cx0Odccx0OO0Oxdk000Oxc:xN   "},
+		{"       WNWWk;',,;oO000000kkO0kdc;;:d00Oxc;;:clccdOOdc:d00kddxO00000Oxoc:cldxkkOOOOkkxdoc:;;ck0000kl:cx0kllkxc:d00Okxoccclok00Oo:lkkxk00OO0000kl:oK    "},
+		{"           Nx,.';:oO000000Oxl:;,;;lk000OxoccccokO0Od::x0Odc;:codxO00Okxolc:::cccccc:;;;'';;oO000Oxc;:lxo:cxxc:oO000OOkkOO000Od:cxOxok0000000Oo:cOW    "},
+		{"            Nd'.';:dO00000ko;;,'';ck0000OOOkkO0000xc;oO0Odc;,',;:codkOOOkkxddoc:;;:ldkl..;:ldxxkko:;;:::::dkoc:oO0000000000ko:cxOOolk0000000xc:xN     "},
+		{"             Xo..';:d00000Odc;,'.,;oO00000000000Oxc;ck00Od:;cc;''',;:cldxO000ko:ckNW Xl..,,,,;;:::;'';:;;;okkdc:cdkkOOOkxdlccok00klck000000kl:oK      "},
+		{"              Xc..',:x00000ko:;,..,;lxO000000Okdl:;;lk00Oxc:kWXOdl;''',;:cloddl;c0   W0kxdoolc::;lo;.',',;cxOkxdlc:cccc:;,;:oO00Oxccx00000Od:cOW      "},
+		{"               K:..';cx0000Odc;::'.',;cloooooc:;,'';:oddol::kW   WXOdl;''',,;::;c0           WNXXNWOdd;.';:clllllc::coxo,.,:lxxkko:cx00000xc:xN       "},
+		{"                0;..,;ck0000ko:;xOl,...'''',;cdk:..,,,,,,;cdKW       WX0xl;,''''c0                   Nx:;;,,,,,,,,;;dNWk'.',;;;::::cx0000kl:oK        "},
+		{"                WO,..,;ck000Oxc;lKWXOxdooodk0N WkclddxkO0KXW             WNK0xod0N                    WNXKKOkxdolcld0WWKxdoollc;'';:dO00Od:cOW        "},
+		{"                 Wk,..,;lxxdoc:;;kW                                                                              WW         WWNx'.,:clodoc:xN         "},
+		{"                  Wx'..,;;,,,;:okXW                                                                                            Ko:;,'',,;;oK          "},
+		{"                   Nd'.,cldxOKXW                                                                                                WWX0kdlclxKW          "},
+		{"                    N0OKNW                                                                                                             WW             "}
+	};
+
+	for (int i = 0; i < 28; ++i) {
+		for (int j = 0; j < 151; ++j) {
+			printf_s("%c", LOGO[i][j]);
+			Sleep(1);
+		}
+		printf_s("\n");
+	}
+
+	Sleep(100);
+	system("color 6");
+	Sleep(100);
+	system("color E");
+	Sleep(100);
+	system("color 6");
+	Sleep(100);
+	system("color E");
+	Sleep(100);
+	system("color 6");
+	Sleep(100);
+	system("color E");
+	Sleep(100);
+	system("color 6");
+	Sleep(100);
+	system("color E");
 	Sleep(500);
-	FirstScene();
+
+
+	system("mode con: cols=150 lines=60");
+	system("color 7");
+
+	VisibleCursor();
+
 }
 
-void MenuScene (OBJECT* _Player) {
+void MenuScene(OBJECT* _Player) {
 	printf_s("MenuScene\n");
 
-	
+
 
 	printf_s("%s \n", _Player->user.info[0]->Name);
 	printf_s("%d \n", _Player->user.info[0]->Att);
@@ -194,7 +255,7 @@ void FirstScene() {
 	printf_s("대사변경테스트 1\n");
 	Sleep(3000);
 	printf_s("대사변경테스트 2");
-	
+
 }
 
 void FirstPokemon(OBJECT* _Player) {
@@ -214,7 +275,7 @@ void FirstPokemon(OBJECT* _Player) {
 	switch (i) {
 	case 1:
 		printf_s("%d", i);
-		_Player->user.info[0]->Name = pname1;		
+		_Player->user.info[0]->Name = pname1;
 		_Player->user.info[0]->Att = 4;
 		_Player->user.info[0]->Def = 4;
 		_Player->user.info[0]->EXP = 0;
@@ -232,7 +293,7 @@ void FirstPokemon(OBJECT* _Player) {
 	case 3:
 		SceneState++;
 		break;
-	default :
+	default:
 		FirstPokemon(_Player);
 		break;
 	}
@@ -242,7 +303,7 @@ void FirstPokemon(OBJECT* _Player) {
 void StageScene(OBJECT* _Player) {
 	// ** 전투
 	PlayerScene(_Player);
-	
+
 }
 
 void InitializePlayer(OBJECT* _Player) {
@@ -250,26 +311,26 @@ void InitializePlayer(OBJECT* _Player) {
 
 }
 
-DWORD SetnameTime = 0;
-
 void PlayerScene(OBJECT* _Player) {
-	int i = SelectMotion();
 
-	switch (i) {
-	case 1 :
-		Battle(_Player);
-		break;
-	case 2 :
-		RecoveryPokemon(_Player);
-		break;
-	case 3 :
-		break;
-	case 4 :
-		break;
+	while (true) {
+		int i = SelectMotion();
 
-
+		switch (i) {
+		case 1:
+			Battle(_Player);
+			break;
+		case 2:
+			RecoveryPokemon(_Player);
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		}
 	}
-	
+
+
 }
 
 int SelectMotion() {
@@ -289,14 +350,14 @@ int SelectMotionBattle() {
 }
 
 void Battle(OBJECT* _Player) {
-	
+
 	printf_s("배틀\n");
 
 	OBJECT* Enemy = (OBJECT*)malloc(sizeof(OBJECT));
 	InitializeEnemy(Enemy);
 
 	while (true) {
-		
+
 		int i = SelectMotionBattle();
 
 		switch (i) {
@@ -474,12 +535,12 @@ void CatchPokemon(OBJECT* _Player, OBJECT* _Enemy) {
 
 		int select = SelectPokemon(_Player);
 		InsertPokemon(_Player, _Enemy, select);
-		
+
 		Sleep(500);
 	}
 	else { // 0일경우
 		printf_s("도망치는것에 [실패] 했습니다.\n");  // 도망치는것에 실패, 몬스터에게 데미지를 입음
-		
+
 		Sleep(500);
 	}
 
@@ -488,37 +549,38 @@ void CatchPokemon(OBJECT* _Player, OBJECT* _Enemy) {
 int SelectPokemon(OBJECT* _Player) {
 	int select = 1;
 
-	printf_s("1. %s \n", _Player->user.info[0]->Name);
+
 	if (_Player->user.info[1] != nullptr) {
 		printf_s("2. %s \n", _Player->user.info[1]->Name);
 		select++;
 	}
-		
+
 	if (_Player->user.info[2] != nullptr) {
 		printf_s("3. %s \n", _Player->user.info[2]->Name);
 		select++;
 	}
-		
+
 	if (_Player->user.info[3] != nullptr) {
 		printf_s("4. %s \n", _Player->user.info[3]->Name);
 		select++;
 	}
-		
+
 	if (_Player->user.info[4] != nullptr) {
 		printf_s("5. %s \n", _Player->user.info[4]->Name);
 		select++;
 	}
-		
+
 	if (_Player->user.info[5] != nullptr) {
 		printf_s("6. %s \n", _Player->user.info[5]->Name);
 		select++;
 	}
-		
+
 	if (select >= 5) {
 		printf_s("저장할 위치 선택 : ");
 		scanf_s("%d", &select);
+		select -= 1;
 	}
-	
+
 
 	return select;
 }
@@ -538,10 +600,13 @@ void InsertPokemon(OBJECT* _Player, OBJECT* _Enemy, int select) {
 	_Player->user.info[select]->speed = _Enemy->user.info[0]->speed;
 
 	free(_Enemy);
+
+	SceneState = 2;
+	StageScene(_Player);
 }
 
 void SwitchPokemon(OBJECT* _Player) {
-	int select = 1;
+	int select = 0;
 
 	printf_s("1. %s \n", _Player->user.info[0]->Name);
 	if (_Player->user.info[1] != nullptr) {
@@ -593,9 +658,9 @@ int RunBattle() {
 
 }
 
-void RecoveryPokemon(OBJECT*  _Player) {
+void RecoveryPokemon(OBJECT* _Player) {
 
-	 _Player->user.info[0]->HP = _Player->user.info[0]->MAXHP;
+	_Player->user.info[0]->HP = _Player->user.info[0]->MAXHP;
 	if (_Player->user.info[1] != nullptr) {
 		_Player->user.info[1]->HP = _Player->user.info[1]->MAXHP;
 	}
@@ -637,11 +702,6 @@ void InitializeEnemy(OBJECT* _Enemy) {
 	_Enemy->user.info[0]->speed = 5;
 }
 
-void EnemyScene(OBJECT* _Enemy) {
-
-}
-
-
 
 void SetPosition(int _x, int _y, char* _str, int _Color) {
 	COORD Pos = { _x, _y };
@@ -678,11 +738,9 @@ void InvisibleCursor() {
 }
 
 /*
-
 	while (true) {
 		// ** 콘솔창을 모두 지움.
 		system("cls");
-
 		printf_s("\n[%s]\n", Objects[PLAYER]->Info.Name);
 		printf_s("HP : %d\n", Objects[PLAYER]->Info.HP);
 		printf_s("MP : %d\n", Objects[PLAYER]->Info.MP);
@@ -692,7 +750,6 @@ void InvisibleCursor() {
 		printf_s("Level : %d\n\n", Objects[PLAYER]->Info.Level);
 		// 0.5초의 딜레이
 		Sleep(500);
-
 		printf_s("[%s]\n", Objects[ENEMY]->Info.Name);
 		printf_s("HP : %d\n", Objects[ENEMY]->Info.HP);
 		printf_s("MP : %d\n", Objects[ENEMY]->Info.MP);
@@ -700,7 +757,6 @@ void InvisibleCursor() {
 		printf_s("방어력 : %.2f\n", Objects[ENEMY]->Info.Def);
 		printf_s("EXP : %d\n", Objects[ENEMY]->Info.EXP);
 		printf_s("Level : %d\n\n", Objects[ENEMY]->Info.Level);
-
 		Sleep(500);
 		// 선택지
 		int iChoice = 0;
@@ -730,11 +786,9 @@ void InvisibleCursor() {
 				printf_s("1의 데미지를 몬스터에게 받았습니다.\n");
 				Sleep(500);
 			}
-
 			break;
 		case 2 : // '2'일경우
 			dice = (int)rand % 2; // 랜덤함수를 이용해 0, 1의 값이 나오게함
-
 			if (dice) { // 1일경우
 				printf_s("도망치는것에 [성공] 했습니다.\n");
 				InitializeObject(Objects[ENEMY], ENEMY); // 도망치는것을 성공, 새로운 몬스터를 등장시킴
@@ -752,9 +806,7 @@ void InvisibleCursor() {
 				}
 				Sleep(500);
 			}
-
 			break;
-
 		default : // 다른 숫자를 입력했을경우
 			printf_s("잘못된 입력입니다. 다시 입력하세요. \n");
 			Sleep(500);
